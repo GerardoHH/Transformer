6682ddd6a97cfcd00c2139622d950d607af645eb
brujo <brujo@MSI>
2025-11-14 15:14:36 -0600

    desarrollo

---
+++ model/custom/attention/GlobalSelfAttention.py
@@ -0,0 +1,11 @@
+from model.custom.attention.BaseAttention import BaseAttention
+
+class GlobalSelfAttention(BaseAttention):
+  def call(self, x):
+    attn_output = self.mha(
+        query=x,
+        value=x,
+        key=x)
+    x = self.add([x, attn_output])
+    x = self.layernorm(x)
+    return x
